b1d47b5938d03e66a34f787596b96c4b900f9185
Ubuntu <ubuntu@ip-172-26-11-106.ap-southeast-1.compute.internal>
2019-07-09 06:29:41 +0000

    add barrons

--- barron/barron/spiders/barronfetch.py
+++ barron/barron/spiders/barronfetch.py
@@ -8,8 +8,12 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
+from scrapy.shell import inspect_response
+import sys
+import io
 
 
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 class BarronfetchSpider(scrapy.Spider):
     name = 'barronfetch'
     custom_settings = {
@@ -21,7 +25,7 @@ class BarronfetchSpider(scrapy.Spider):
     header = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
             }
-    start_urls = ['https://accounts.barrons.com/login?opts=0&target=https%3A%2F%2Fwww.barrons.com%2F']
+    start_urls = ['https://www.barrons.com/']
     def parse(self, response):
         options = webdriver.FirefoxOptions()
         options.add_argument('-headless')
@@ -37,19 +41,15 @@ class BarronfetchSpider(scrapy.Spider):
         finally:
             cookie = driver.get_cookies()
             print(cookie,'+++++++++++++++++++++++++++++++++')
-            sreach_window=driver.current_window_handle
-            time.sleep(5)
-            driver.find_element_by_id("brand-label").click()
             driver.quit()
-            yield scrapy.Request(url='https://www.barrons.com/',callback=self.parse_list,cookies=cookie,headers=BarronfetchSpider.header,meta={'dont_cache':True})
-    def parse_list(self,response):
-        data = pd.read_csv('output.csv', sep='~')
-        for i in range(len(data)):
-            link = data.url[i]
-            category = data.category[i]
-            subtitle = data.subtitle[i]
-            yield scrapy.Request(url = link,callback = self.parse_body,meta = {'subtitle':subtitle,'category':category})
+            data = pd.read_csv('output.csv', sep='~')
+            for i in range(len(data)):
+                link = data.url[i] + '?mod=hp_LEAD_1'
+                category = data.category[i]
+                subtitle = data.subtitle[i]
+                yield scrapy.Request(url = link,callback = self.parse_body,cookies=cookie,headers=BarronfetchSpider.header,meta = {'subtitle':subtitle,'category':category})
     def parse_body(self,response):
+#        inspect_response(response, self)
         item = BarronItem()
         item['url'] = response.url
         item['title'] = response.xpath("//meta[@name='article.headline']/@content").extract_first()
@@ -59,8 +59,9 @@ class BarronfetchSpider(scrapy.Spider):
         item['date'] = ' '.join(datetimes.strip().split(' ')[1:4]) if datetimes.strip().split(' ')[0] == 'Updated' else ' '.join(datetimes.strip().split(' ')[:3])
         tag = response.xpath("//meta[@name='keywords']/@content").extract_first()
         item['tags'] = tag
-        bodys = response.xpath('//*[@class="snippet__body"]//p//text()').extract()
+        bodys = response.xpath('//*[@id="js-article__body"]//p//text()').extract()
         b = ''.join(bodys)
+        item['body'] = b
         re_h =re.compile(r'\s+')
         item['body'] = re_h.sub(' ',b).replace('\ufeff',' ')
         tags = tag if tag else 'nan'
